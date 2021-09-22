0f359c6c28467f581ab10a3b3d6d7ab8f976b2de
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2021-09-22 10:09:32 -0400

    Update Review Mock Data

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,30 +1,30 @@
 const reviews = [
 	{
 		id: 1,
-		username: 'grace',
+		username: 'gxjcy',
 		rating: 5,
 		date: '11/22/2020',
-		title: 'Best app ever',
+		title: 'Best thing ever',
 		description:
 			'I can pick between lots of things and I can make up my own math problems also with the arrow you can play spin the bottle thank you',
 	},
 	{
 		id: 2,
-		username: 'grace2',
+		username: 'camicamz10',
 		rating: 4,
 		date: '07/07/2021',
 		title: 'Great tool',
 		description:
-			'It’s like it said: “fast & simple”! And it doesn’t require a lot of memory space like some other similar App’s do. I’d strongly recommend this App.',
+			'The app itself is great and fine! I have a lot of trouble making simple decisions so this really helps. But one more thing to add to the random number generator would be the use of negative numbers I dont really see anyone else use those so it would be wonderful to have an already great app use that features',
 	},
 	{
 		id: 3,
-		username: 'grace3',
-		rating: 4.5,
+		username: 'hrjchdbfhjhfjhdh',
+		rating: 5,
 		date: '01/24/2021',
-		title: 'Pretty good',
+		title: 'Great tool',
 		description:
-			'No glitches. No adds. And I could decide all my math facts. The app does its job, which is all I need to say.',
+			'It’s like it said: “fast & simple”! And it doesn’t require a lot of memory space like some other similar App’s do. I’d strongly recommend this App.',
 	},
 ];
 
