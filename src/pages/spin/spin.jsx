775e54f5b03346b7e48876b09a3c11d80d9d6ce0
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2021-10-04 13:35:39 -0400

    [RP-31] Spin - Create UI
    https://29ground.atlassian.net/browse/RP-31

--- src/pages/spin/spin.jsx
+++ src/pages/spin/spin.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+
+import Button from '../../shared-component/button/button';
+
+import './spin.css';
 
 const Spin = (props) => {
-	return <h1>Spin</h1>;
+	return (
+		<div className='spin'>
+			<FaArrowUp className='arrow' />
+			<Button mode>spin</Button>
+		</div>
+	);
 };
 
 export default Spin;
